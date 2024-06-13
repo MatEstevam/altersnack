779364b66f3e3d12e4849127e176295a2e344313
Pedro Guimaraes <peuvictor22@gmail.com>
2024-06-13 10:23:42 -0300

    resolvendo bugs

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
+  has_many :product_restrictions, dependent: :destroy
+  has_many :restrictions, through: :product_restrictions
 
   has_one_attached :photo
 
