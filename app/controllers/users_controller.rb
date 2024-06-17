1ed390c74feae4ef42e5043ced71126bf00caa9a
Matheus Estevam <matheus.estevam.br@gmail.com>
2024-06-17 09:50:10 -0300

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, photo: [], adress: [])
+    params.require(:user).permit(:name, :email, :photo, :address)
   end
 end
