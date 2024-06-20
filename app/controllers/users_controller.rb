8b03162dc7802471c5ef884b972f02614af41e1f
Pedro Guimaraes <peuvictor22@gmail.com>
2024-06-20 09:15:30 -0300

    raetaurant page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,22 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-  before_action :set_user, only: [:show]
-  before_action :authorize_user, only: [:show]
+  before_action :set_user, only: :show
+  before_action :authorize_user, only: :show
 
   def show
-    @user = User.find(params[:id])
-    if @user.restaurant
-      @orders = Order.joins(:product).where(products: { user_id: @user.id })
+    if @user.restaurant?
+      @orders = Order.joins(cart: { cart_items: :product }).where(products: { user_id: @user.id })
     else
-      @orders = @user.orders.includes(:product)
+      @orders = @user.orders.includes(cart: { cart_items: :product })
     end
-    authorize @user
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
-    redirect_to root_path, alert: 'User not found' unless @user
   end
 
   def authorize_user
-    redirect_to root_path, alert: 'Not authorized' unless current_user == @user
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :photo, :address)
+    authorize @user
   end
 end
